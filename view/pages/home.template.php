4d208d6a80ed25abc40df20bd9b8fef5823ea232
Bóka Tamás <tamasboka2006@gmail.com>
2025-10-14 19:28:02 +0200

    added cards and title

--- view/pages/home.template.php
+++ view/pages/home.template.php
@@ -1,1 +1,40 @@
+<h1 class="title is-1 is-justify-content-center is-flex mt-3">Welcome!</h1>
+<h4 class="subtitle is-4 is-justify-content-center is-flex mt-3">Go and explore our site!</h4>
+<div class="container mt-6">
+    <div class="columns">
+        <div class="column is-4">
+            <div class="card">
+                <div class="card-header">
+                    <div class="card-header-title is-justify-content-center is-flex">
+                        Check out our playlist!
+                    </div>
+                </div>
+                <div class="card-content is-justify-content-center is-flex">
+                    <div class="content">
+                        <a class="button is-link is-dark" href="?page=playlist">Go to playlist page</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="column is-4">
+            <p class="is-justify-content-center is-flex">OR</p>
+        </div>
+        <div class="column is-4">
+            <div class="card">
+                <div class="card-header">
+                    <div class="card-header-title is-justify-content-center is-flex">
+                        Add a new track!
+                    </div>
+                </div>
+                <div class="card-content is-justify-content-center is-flex">
+                    <div class="content">
+                        <div class="content">
+                            <a class="button is-link is-dark" href="?page=add_track">Go to add track page</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 <?php
