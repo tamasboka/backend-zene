31dcd9e481a7f1b6e8a28bf5b11404b9fb76b17e
Bóka Tamás <tamasboka2006@gmail.com>
2025-10-14 18:55:17 +0200

    added filter_tracks_by_genre, get_total_playlist_duration and get_tracks_by_decade functions

--- app/functions.php
+++ app/functions.php
@@ -4,5 +4,36 @@
  * @return mixed
  */
 function get_all_tracks(){
-    return json_decode(file_get_contents(__DIR__ . '/data.json'));
+    return json_decode(file_get_contents(__DIR__ . '/data.json'), true);
+}
+function filter_tracks_by_genre($tracks, $genre){
+    $filtered_tracks = [];
+    foreach ($tracks as $track){
+        if ($track["genre"] === $genre){
+            $filtered_tracks[] = $track;
+        }
+    }
+    return $filtered_tracks;
+}
+function get_total_playlist_duration($tracks){
+    $total_duration = 0;
+    foreach ($tracks as $track){
+        $total_duration += $track["length"];
+    }
+    return $total_duration;
+}
+function get_tracks_by_decade($tracks, $decade){
+    $filtered_tracks = [];
+    foreach ($tracks as $track){
+        $track_year = strval($track["year"])[0].strval($track["year"])[1].strval($track["year"])[2];
+
+        //FOR DEBUGGING PURPOSES
+        //echo "track:".$track_year."<br>";
+        //echo "decade:".$decade[0].$decade[1].$decade[2]."<br>";
+
+        if ($track_year === $decade[0].$decade[1].$decade[2]){
+            $filtered_tracks[] = $track;
+        }
+    }
+    return $filtered_tracks;
 }
