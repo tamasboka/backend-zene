6b7141d4d0e5e7167ea27d3334ccfac6c043e905
Bóka Tamás <tamasboka2006@gmail.com>
2025-10-14 22:35:21 +0200

    added checking for filters

--- index.php
+++ index.php
@@ -6,7 +6,19 @@ $current_page = $_GET["page"] ?? "home";
 
 require_once "view/partials/header.template.php";
 
+if ($current_page === "playlist"){
+    $used_tracks = $tracks;
+    if (isset($_GET["genre"]) && isset($_GET["decade"])) {
+        if ($_GET["genre"] != "All") {
+            $used_tracks = filter_tracks_by_genre($tracks, $_GET["genre"]);
+        }
+        if ($_GET["decade"] != "All") {
+            $used_tracks = get_tracks_by_decade($used_tracks, intval($_GET["decade"]));
+        }
+    }
+}
+
 require "view/pages/".$current_page.".template.php";
 
-require_once "view/partials/footer.template.php";
+require "view/partials/footer.template.php";
 ?>
