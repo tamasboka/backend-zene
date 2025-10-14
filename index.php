2ce6b9519760829eda7bf231d8dcab21fd18af60
Bóka Tamás <tamasboka2006@gmail.com>
2025-10-14 18:57:15 +0200

    called php files

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
 <?php
+require_once "app/functions.php";
+require_once "app/data.php";
+
+$current_page = $_GET["page"] ?? "home";
+
+require_once "view/partials/header.template.php";
+
+require "view/pages/".$current_page.".template.php";
+
+require_once "view/partials/footer.template.php";
+?>
