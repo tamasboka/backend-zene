b1f7103a2fd029fda91a74c750f8b2635a4cbe71
Bóka Tamás <tamasboka2006@gmail.com>
2025-10-14 18:54:42 +0200

    added footer that consists of 3 colums

--- view/partials/footer.template.php
+++ view/partials/footer.template.php
@@ -1,1 +1,16 @@
-<?php
+</main>
+<footer class="footer mt-0">
+    <div class="content has-text-centered">
+        <div class="columns">
+            <div class="column is-4">
+
+            </div>
+            <div class="column is-4">
+
+            </div>
+            <div class="column is-4">
+
+            </div>
+        </div>
+    </div>
+</footer>
