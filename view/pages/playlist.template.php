44fb62131160bf08c2f5b0fcd273f1aeebccc183
Bóka Tamás <tamasboka2006@gmail.com>
2025-10-14 21:53:46 +0200

    added columns for table and filter menu, made a filter menu

--- view/pages/playlist.template.php
+++ view/pages/playlist.template.php
@@ -4,9 +4,45 @@ require "app/data.php";
 ?>
 
 <h1 class="title is-1 is-justify-content-center is-flex mt-3">Playlist</h1>
-<div class="constainer is-justify-content-center is-flex">
-    <table class="table">
-        <thead>
+
+<div class="container is-justify-content-center is-flex">
+    <div class="columns is-2">
+        <form method="get" style="width: 75%" class="mt-4">
+            <input hidden="hidden" name="page" value="playlist">
+            <h3 class="is-justify-content-center is-flex title">Filter songs</h3>
+            <div class="box">
+                <div class="field">
+                    <label for="genre">Genre</label>
+                    <div class="control">
+                        <input id="genre" class="input is-rounded" type="text" name="genre">
+                    </div>
+                </div>
+                <div class="field">
+                    <label for="decade">Decade</label>
+                    <div class="control">
+                        <div class="select is-rounded">
+                            <select name="decade" id="decade">
+                                <?php
+                                    $decade = 1970;
+                                    while ($decade < 2030) {
+                                        echo "<option>$decade</option>";
+                                        $decade += 10;
+                                    }
+                                ?>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+                <button class="button is-rounded is-rounded is-primary is-dark mt-3" type="submit">Apply filters</button>
+            </div>
+        </form>
+    </div>
+    <div class="columns is-2" style="background-color: #ffffff">
+        <p>&nbsp;</p>
+    </div>
+    <div class="column is-8">
+        <table class="table">
+            <thead>
             <tr>
                 <th>Title</th>
                 <th>Performer</th>
@@ -15,13 +51,14 @@ require "app/data.php";
                 <th>Genre</th>
                 <th>Year</th>
             </tr>
-        </thead>
-        <tbody>
-        <?php
-        foreach ($tracks as $track) {
-            echo render_track_row($track);
-        }
-        ?>
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+            <?php
+            foreach ($tracks as $track) {
+                echo render_track_row($track);
+            }
+            ?>
+            </tbody>
+        </table>
+    </div>
 </div>
