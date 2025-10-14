c76fa564330f99dd40f5485ebd5e22276c07860f
Bóka Tamás <tamasboka2006@gmail.com>
2025-10-14 21:03:39 +0200

    created table of tracks

--- view/pages/playlist.template.php
+++ view/pages/playlist.template.php
@@ -1,1 +1,27 @@
 <?php
+require "app/view_functions.php";
+require "app/data.php";
+?>
+
+<h1 class="title is-1 is-justify-content-center is-flex mt-3">Playlist</h1>
+<div class="constainer is-justify-content-center is-flex">
+    <table class="table">
+        <thead>
+            <tr>
+                <th>Title</th>
+                <th>Performer</th>
+                <th>Album</th>
+                <th>Length</th>
+                <th>Genre</th>
+                <th>Year</th>
+            </tr>
+        </thead>
+        <tbody>
+        <?php
+        foreach ($tracks as $track) {
+            echo render_track_row($track);
+        }
+        ?>
+        </tbody>
+    </table>
+</div>
