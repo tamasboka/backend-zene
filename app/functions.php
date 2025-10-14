104f1b1bcf6d6c2f4453482d6d6688af4720cac5
Bóka Tamás <tamasboka2006@gmail.com>
2025-10-14 17:47:47 +0200

    added get_all_tracks function

--- app/functions.php
+++ app/functions.php
@@ -1,1 +1,8 @@
 <?php
+/**
+ * Reads all data from data.json and returns it.
+ * @return mixed
+ */
+function get_all_tracks(){
+    return json_decode(file_get_contents(__DIR__ . '/data.json'));
+}
