b0a45cdb3ce8eeb413428bf250b0207f2eeea8a9
Bóka Tamás <tamasboka2006@gmail.com>
2025-10-14 17:52:24 +0200

    added navbar, bulma css

--- view/partials/header.template.php
+++ view/partials/header.template.php
@@ -1,1 +1,22 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Zene</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@1.0.4/css/bulma.min.css">
+    <style>
+
+    </style>
+</head>
+<header>
+    <nav class="navbar">
+        <div class="container">
+            <div class="navbar-menu is-flex is-justify-content-center">
+                <a class="navbar-item <?php if ($current_page === "home") echo "has-background-link-dark"?>" href="?page=home">Home</a>
+                <a class="navbar-item <?php if ($current_page === "playlist") echo "has-background-link-dark"?>" href="?page=playlist">Playlist</a>
+                <a class="navbar-item <?php if ($current_page === "add_track") echo "has-background-link-dark"?>" href="?page=add_track">Add track</a>
+            </div>
+        </div>
+    </nav>
+</header>
+<body>
