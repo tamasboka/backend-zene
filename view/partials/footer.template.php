e738565c0b5883a6060bbd2032e27bb91b00b714
Bóka Tamás <tomi500offical@gmail.com>
2025-10-25 22:39:46 +0200

    added name and GitHub repo into footer

--- view/partials/footer.template.php
+++ view/partials/footer.template.php
@@ -2,14 +2,15 @@
 <footer class="footer mt-0">
     <div class="content has-text-centered">
         <div class="columns">
-            <div class="column is-4">
-
+            <div class="column is-6">
+                <p class="title">Made by</p>
+                <p>Bóka Tamás</p>
             </div>
-            <div class="column is-4">
-
-            </div>
-            <div class="column is-4">
-
+            <div class="column is-6">
+                <p class="title">
+                    GitHub repository
+                </p>
+                <a class="button is" href="https://github.com/tamasboka/backend-zene" target="_blank">backend-zene</a>
             </div>
         </div>
     </div>
