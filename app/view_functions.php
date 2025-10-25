53dc45a8e53806fea4dd7d30a35efeeeb8d4bb27
Bóka Tamás <tomi500offical@gmail.com>
2025-10-25 20:52:56 +0200

    format_duration can now format into hh:mm:ss too

--- app/view_functions.php
+++ app/view_functions.php
@@ -9,16 +9,31 @@ function render_track_row($track) {
                  <td>".$track["year"]."</td>
               </tr>";
 }
-function format_duration($seconds) {
-    $minutes = floor($seconds / 60);
+function format_duration($seconds): string
+{
+    $hours = floor($seconds / 3600);
+    $minutes = floor(($seconds % 3600) / 60);
     $seconds = $seconds % 60;
-    if (strlen($seconds) == 1) {
-        $seconds = "0".$seconds;
+
+    $format_time = "";
+    if ($hours != 0) {
+        if (strlen($hours) == 1) {
+            $hours = "0".$hours;
+        }
     }
     if (strlen($minutes) == 1) {
         $minutes = "0".$minutes;
     }
-    return "$minutes:$seconds";
+    if (strlen($seconds) == 1) {
+        $seconds = "0".$seconds;
+    }
+    if ($hours != 0) {
+        $format_time .= $hours.":".$minutes.":".$seconds;
+    } else {
+        $format_time .= $minutes.":".$seconds;
+    }
+
+    return $format_time;
 }
 function render_genre_tag($genre){
     return "<span class='tag is-rounded is-large'>$genre</span>";
