cc8d7d06bfe507fd14c4fff4aad819d10178748c
Bóka Tamás <tomi500offical@gmail.com>
2025-10-25 20:53:20 +0200

    swapped home and playlist in navbar

--- view/partials/header.template.php
+++ view/partials/header.template.php
@@ -20,8 +20,8 @@
     <nav class="navbar">
         <div class="container">
             <div class="navbar-menu is-flex is-justify-content-center">
-                <a class="navbar-item <?php if ($current_page === "home") echo "has-background-link-dark"?>" href="?page=home">Home</a>
                 <a class="navbar-item <?php if ($current_page === "playlist") echo "has-background-link-dark"?>" href="?page=playlist">Playlist</a>
+                <a class="navbar-item <?php if ($current_page === "home") echo "has-background-link-dark"?>" href="?page=home">Home</a>
                 <a class="navbar-item <?php if ($current_page === "add_track") echo "has-background-link-dark"?>" href="?page=add_track">Add track</a>
             </div>
         </div>
